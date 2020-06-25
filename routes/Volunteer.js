641300b5a9047f2302422ccf7b1b26001a20853b
devindersingh79904 <devindersingh79904@gmail.com>
2020-06-25 22:27:50 +0530

    increment of function of duties is impelement

--- routes/Volunteer.js
+++ routes/Volunteer.js
@@ -1,26 +1,25 @@
-const express = require("express");
-const Volunteer = require("../models/Volunteer");
-const config = require("config");
-
+const express = require('express');
+const Volunteer = require('../models/Volunteer');
+const config = require('config');
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     result = await Volunteer.find();
     return res.json(result);
   } catch (err) {
-    res.status(501).json({ msg: "Server error" });
+    res.status(501).json({ msg: 'Server error' });
   }
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const { rollno, name, clas, batch, email, phnno } = req.body;
 
   try {
     //console.log("Duties : " + dutiesAssigned);
     var user = await Volunteer.findOne({ email });
     if (user) {
-      return res.json({ msg: "User Already register" });
+      return res.json({ msg: 'User Already register' });
     }
 
     var newUser = new Volunteer({
@@ -37,34 +36,86 @@ router.post("/", async (req, res) => {
 
     await newUser.save();
     //return res.json(newUser);
-    return res.json({ msg: "Successfully Added" });
+    return res.json({ msg: 'Successfully Added' });
   } catch (error) {
-    console.error("error ", error.message);
-    res.status(501).json({ msg: "Server error" });
+    console.error('error ', error.message);
+    res.status(501).json({ msg: 'Server error' });
   }
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   const _id = req.params.id;
-  console.log("params : ", _id);
+  console.log('params : ', _id);
 
   try {
-    const vol = await Volunteer.findById(req.params.id);
+    var vol = await Volunteer.findById(req.params.id);
     if (!vol) {
-      return res.status(401).json("No volunteer found.");
+      return res.status(401).json('No volunteer found.');
     }
     await Volunteer.findByIdAndRemove(req.params.id);
-    res.json({ msg: "deleted Succussfuly" });
+    res.json({ msg: 'deleted Succussfuly' });
   } catch (error) {
     console.error(error.message);
-    res.status(501).json({ msg: "Server error . " });
+    res.status(501).json({ msg: 'Server error . ' });
   }
 });
 
-router.put("/dasign/:id", (req, res) => {});
+router.put('/dasign/:id', async (req, res) => {
+  try {
+    var volunteer = await Volunteer.findById(req.params.id);
+    if (!volunteer) {
+      return res.json({ msg: 'volunteer not found.' });
+    }
 
-router.put("/daccept/:id", (req, res) => {});
+    volunteer = await Volunteer.findByIdAndUpdate(req.params.id, {
+      $inc: {
+        dutiesAssigned: 1,
+      },
+    });
+    return res.json(volunteer);
+  } catch (err) {
+    console.error(err.message);
 
-router.put("/work/:id", (req, res) => {});
+    res.status(500).json({ msg: 'Server error' });
+  }
+});
+
+router.put('/daccept/:id', async (req, res) => {
+  try {
+    var volunteer = await Volunteer.findById(req.params.id);
+    if (!volunteer) {
+      return res.json({ msg: 'volunteer not found.' });
+    }
+
+    volunteer = await Volunteer.findByIdAndUpdate(req.params.id, {
+      $inc: {
+        dutiesAccepted: 1,
+      },
+    });
+    return res.json(volunteer);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server error' });
+  }
+});
+
+router.put('/work/:id', async (req, res) => {
+  try {
+    var volunteer = await Volunteer.findById(req.params.id);
+    if (!volunteer) {
+      return res.json({ msg: 'volunteer not found.' });
+    }
+
+    volunteer = await Volunteer.findByIdAndUpdate(req.params.id, {
+      $inc: {
+        workDone: 1,
+      },
+    });
+    return res.json(volunteer);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server error' });
+  }
+});
 
 module.exports = router;
