7a9bf284ef93843ce3a423a01f70ad20b07b0ac9
devindersingh79904 <devindersingh79904@gmail.com>
2020-06-26 18:19:53 +0530

    Home page and about page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Navbar from './component/layout/Navbar';
+import Home from './component/pages/Home';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <div class='page-wrapper'>
+        <div class='container-fluid'>
+          <Home />
+        </div>
+      </div>
     </div>
   );
 }
