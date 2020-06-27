8d303844f3019379403a7275711a21312341ae51
Sarv1997 <singhsarvpreet14@gmail.com>
2020-06-28 03:35:58 +0530

    Update Navbar.js

--- client/src/component/layout/Navbar.js
+++ client/src/component/layout/Navbar.js
@@ -28,11 +28,6 @@ const Navbar = ({ auth: { isAuthenticated, loading, user }, logout }) => {
               </Link>
             </li>
             <li>
-              <Link to='/about' className='connection-item'>
-                <span className='block'>About</span>
-              </Link>
-            </li>
-            <li>
               <Link to='/login' className='connection-item'>
                 <i className='pe-7s-unlock txt-success'></i>
                 <span className='block'>Login</span>
